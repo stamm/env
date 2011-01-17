5c5c2a92ffe5adc23fec7038bf644345548964c0
Rustam Zagirov <rustam@zagirov.name>
2011-01-18 00:17:01 +0300

    Separate blocks of code to functions

--- server/debian/first_install.sh
+++ server/debian/first_install.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 #Config of locales and time zones
-dpkg-reconfigure locales tzdata
+#dpkg-reconfigure locales tzdata
+install_progs() {
+	apt-get update
+	apt-get install aptitude git-core htop ntpdate zip rar unrar bash-completion
+}
 
+upgrade_dist() {
+	my_cp "/etc/apt/apt.conf.d/99cache"
+	my_cp "/etc/apt/sources.list.d/"
+	my_cp "/etc/apt/preferences"
+exit;
+	apt-get update
+	apt-get -y upgrade
+	apt-get -y dist-upgrade
+	apt-get clean
+	apt-get autoclean
+	apt-get autoremove
+}
 
-apt-get update
-apt-get install aptitude git-core
+my_cp(){
+echo cp ./files$1 $1
+}
 
-
+upgrade_dist
